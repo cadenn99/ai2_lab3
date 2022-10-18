95859d16b79a9f0602a8cd5d3f37ba1cbba157dd
cadenn99 <104002686+cadenn99@users.noreply.github.com>
2022-10-18 15:19:30 +0200

    comments

--- kohonen.py
+++ kohonen.py
@@ -32,8 +32,8 @@ class Kohonen:
         # A 2-dimensional list of clusters. Size == N x N
         self.clusters = [[Cluster(dim, i, j) for i in range(n)] for j in range(n)]
         # Threshold above which the corresponding html is prefetched
-        self.prefetch_threshold = 0.35
-        self.initial_learning_rate = 0.9
+        self.prefetch_threshold = 0.5
+        self.initial_learning_rate = 0.8
         # The accuracy and hitrate are the performance metrics (i.e. the results)
         self.accuracy = 0
         self.hitrate = 0
